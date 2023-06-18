f3533edd506f5ae80b1d78947e8dddda8820163e
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2023-06-18 16:45:29 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+function imgslider(any)
+{
+    document.querySelector('.pepsi').src=any;
+}
+
+function clrslider(color)
+{
+    const sec= document.querySelector('.sec');
+    sec.style.background=color;
+}
